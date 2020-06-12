13a26bc3259117061606db846f3e1062f84c0883
Dave McPherson <dave@insuredbyus.com>
2020-06-12 15:31:02 +1000

    Ninja 10 Hands on 6 solution

---
+++ ninja010/006/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//     write a program that
+// puts 100 numbers to a channel
+// pull the numbers off the channel and print them
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for v := range c {
+		fmt.Println(v)
+	}
+}
