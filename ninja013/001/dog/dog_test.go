e27c29427d66d80f83fc3d0abc0936a542a4fe02
Dave McPherson <dmcpherson@squareup.com>
2020-06-22 13:46:51 +1000

    Ninja 13 hands on 1 example and test

--- ninja013/001/dog/dog_test.go
+++ ninja013/001/dog/dog_test.go
@@ -1,6 +1,9 @@
 package dog
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func BenchmarkYears(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -11,5 +14,31 @@ func BenchmarkYears(b *testing.B) {
 func BenchmarkYearsTwo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		YearsTwo(12)
+	}
+}
+
+func ExampleYears() {
+	fmt.Println(Years(12))
+	// Output:
+	// 84
+}
+
+func ExampleYearsTwo() {
+	fmt.Println(YearsTwo(12))
+	// Output:
+	// 84
+}
+
+func TestYears(t *testing.T) {
+	x := Years(10)
+	if x != 70 {
+		t.Error("got", x, "expected", 70)
+	}
+}
+
+func TestYearsTwo(t *testing.T) {
+	x := YearsTwo(10)
+	if x != 70 {
+		t.Error("got", x, "expected", 70)
 	}
 }
